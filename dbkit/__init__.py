0bf891ea8ac1228c5ed53c40864900c6fa3ae164
Keith Gaughan <k@stereochro.me>
2012-04-13 15:33:37 +0100

    Basic implementation of the context stack management code along with most of the functions to be exposed by the module.

---
+++ dbkit/__init__.py
@@ -0,0 +1,85 @@
+"""
+"""
+
+import threading
+
+
+__all__ = ['connect']
+
+
+class _Context(object):
+    """
+    """
+
+    __slots__ = ['module', 'conn']
+    state = threading.local()
+
+    def __init__(self, module, conn):
+        super(_Context, self).__init__()
+        self.module = module
+        self.conn = conn
+
+    def __enter__(self):
+        self._push_context(self)
+
+    def __exit__(self, _exc_type, _exc_value, _traceback):
+        self._pop_context()
+
+    @classmethod
+    def _push_context(cls, ctx):
+        cls.state.__dict__.setdefault('stack', [])
+        cls.state.stack.append(ctx)
+        cls.state.current = ctx
+
+    @classmethod
+    def _pop_context(cls):
+        if cls._current() is not None:
+            stack = cls.state.stack
+            stack.pop()
+            if len(stack) == 0:
+                cls.state.current = None
+            else:
+                cls.state.current = stack[len(stack) - 1]
+
+    @classmethod
+    def _current(cls):
+        return cls.state.__dict__.setdefault('current', None)
+
+
+def connect(module, *args, **kwargs):
+    """
+    """
+    conn = module.connect(*args, **kwargs)
+    return _Context(module, conn)
+
+def transaction():
+    """
+    """
+    pass
+
+def execute(query, args):
+    """
+    """
+    pass
+
+def execute_many():
+    """
+    """
+    pass
+
+def query_row(query, args):
+    """
+    """
+    pass
+
+def query_value(query, args):
+    """
+    """
+    pass
+
+def query_row(query, args):
+    """
+    """
+    pass
+
+# vim:set et ai:
