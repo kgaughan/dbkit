4ab4da1687a0abf5d6184a678c36e3f21707a16b
Keith Gaughan <k@stereochro.me>
2012-06-10 03:28:57 +0100

    capture_stderr() no longer does what I need, so getting rid of it.

--- tests/utils.py
+++ tests/utils.py
@@ -6,18 +6,6 @@ import contextlib
 import StringIO
 import sys
 
-
-@contextlib.contextmanager
-def capture_stderr():
-    """Patch sys.stderr to capture anything written to it."""
-    captured = StringIO.StringIO()
-    old_stderr = sys.stderr
-    sys.stderr = captured
-    try:
-        yield captured
-    finally:
-        sys.stderr = old_stderr
-
 def skip_first_line(value):
     """Returns everything after the first newline in the string."""
     parts = value.split("\n", 1)
