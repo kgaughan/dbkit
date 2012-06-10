50a7fdf5ea55a59c2b7da32bc2f7f7c160afe029
Keith Gaughan <k@stereochro.me>
2012-06-10 05:41:21 +0100

    Ensure that resource contention is handled properly in the pool.

--- tests/test_pools.py
+++ tests/test_pools.py
@@ -1,10 +1,11 @@
 from __future__ import with_statement
+import threading
 
 import dbkit
-from tests import fakedb
+from tests import fakedb, utils
 
 
-POOL = dbkit.Pool(fakedb, 2, fakedb.INVALID_CURSOR)
+POOL = dbkit.Pool(fakedb, 1, fakedb.INVALID_CURSOR)
 
 
 def test_check_pool():
@@ -40,3 +41,27 @@ def test_bad_query():
         except ctx.OperationalError:
             # An operational error will close the connection it occurs on.
             assert POOL._allocated == 0
+
+def test_contention():
+    # Here, we're testing that the pool behaves properly when it hits its
+    # maximum number of connections and a thread it waiting for another one
+    # to release the connection it's currently using.
+    release = threading.Event()
+    spawn = threading.Event()
+    def hog_connection():
+        with POOL.connect() as ctx:
+            with dbkit.transaction():
+                spawn.set()
+                release.wait()
+    def wait_on_connection():
+        with POOL.connect() as ctx:
+            spawn.wait()
+            # Request the other thread to release the connection after a
+            # short period, enough to ensure the conditional variable
+            # managing the pool is waited on by this thread. Basically
+            # nearly any pause should be long enough, though 1/100 of a
+            # second seems like a reasonable balance.
+            threading.Timer(1.0/100, lambda: release.set()).start()
+            with dbkit.transaction():
+                pass
+    utils.spawn([wait_on_connection, hog_connection])
