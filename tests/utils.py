e5d3dfb83f9d0dfd7d0316422bd17ba6331b01d1
Keith Gaughan <k@stereochro.me>
2012-06-10 05:32:50 +0100

    Function for spawning a bunch of threads.

--- tests/utils.py
+++ tests/utils.py
@@ -2,11 +2,19 @@
 Utility functions used by the tests.
 """
 
-import contextlib
-import StringIO
-import sys
+import threading
 
 def skip_first_line(value):
     """Returns everything after the first newline in the string."""
     parts = value.split("\n", 1)
     return parts[1] if len(parts) == 2 else ''
+
+def spawn(targets):
+    """Spawns a bunch of threads for given targets and waits on them."""
+    threads = []
+    for target in targets:
+        thread = threading.Thread(target=target)
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
