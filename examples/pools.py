9a8c0d5c99afc951b00effb9dd2097c155293cb4
Keith Gaughan <k@stereochro.me>
2012-06-17 23:28:28 +0100

    Small app built with web.py demonstrating use of connection pools.

---
+++ examples/pools.py
@@ -0,0 +1,57 @@
+import web
+import psycopg2
+import pystache
+from dbkit import Pool, transaction, query, query_value, execute, dict_set
+
+
+urls = (
+    '/(.*)', 'hello'
+)
+app = web.application(urls, globals())
+pool = Pool(psycopg2, 2, "dbname=namecounter user=keith")
+
+
+TEMPLATE = """<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello!</title>
+    </head>
+    <body>
+        <p>Hello, {{name}}!</p>
+        <p>Previously, I've said hello to:</p>
+        <ul>
+        {{#hellos}}
+            <li>{{name}}, {{n}} times</li>
+        {{/hellos}}
+        </ul>
+    </body>
+</html>"""
+
+
+def save_name(name):
+    with transaction():
+        if query_value("SELECT n FROM greeted WHERE name = %s", (name,), 0) == 0:
+            execute("INSERT INTO greeted (name, n) VALUES (%s, 1)", (name,))
+        else:
+            execute("UPDATE greeted SET n = n + 1 WHERE name = %s", (name,))
+
+def get_names():
+    return query("SELECT name, n FROM greeted ORDER BY n", factory=dict_set)
+
+
+class hello(object):
+    def GET(self, name):
+        ctx = pool.connect()
+        if not name:
+            name = 'World'
+        with ctx:
+            hellos = list(get_names())
+            save_name(name)
+        return pystache.render(TEMPLATE, {'name': name, 'hellos': hellos})
+
+
+if __name__ == '__main__':
+    try:
+        app.run()
+    finally:
+        pool.finalise()
