ad43612ff444ac019222353d218db2d7127b6c31
Keith Gaughan <k@stereochro.me>
2012-07-15 13:21:10 +0100

    Added projects.

--- examples/notary/notary.sql
+++ examples/notary/notary.sql
@@ -1,7 +1,14 @@
+CREATE TABLE projects (
+	project_id INTEGER NOT NULL PRIMARY KEY,
+	project    TEXT    NOT NULL
+);
+
 CREATE TABLE notes (
-	id      INTEGER  NOT NULL PRIMARY KEY,
-	note    TEXT     NOT NULL,
-	created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+	note_id    INTEGER  NOT NULL PRIMARY KEY,
+	project_id INTEGER  NOT NULL DEFAULT 0 REFERENCES projects (project_id),
+	note       TEXT     NOT NULL,
+	created    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX ix_note_created ON notes (created);
+CREATE INDEX ix_note_project ON notes (project_id);
