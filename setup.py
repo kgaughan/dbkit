aab2d90bb81a30b593ddff4d25984e3b5ccc236c
Keith Gaughan <keith.gaughan@workday.com>
2019-11-03 19:15:50 +0000

    Remove Python 3.3 as a target.

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
     zip_safe=True,
     test_suite="tests.suite",
     classifiers=[
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
